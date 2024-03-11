fed3ff1683dfec51d1bedd3b321a93d58ef9053c
catarina-pinto <catarinafmpinto@gmail.com>
2024-03-11 12:30:34 +0000

    Initial Commit

---
+++ PinkRoomChallenge/PinkRoomChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinkRoomChallengeApp.swift
+//  PinkRoomChallenge
+//
+//  Created by Catarina Pinto on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinkRoomChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
