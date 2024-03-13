f85a681a44b73596a31b08e49015994ce6e8d909
catarina-pinto <catarinafmpinto@gmail.com>
2024-03-13 15:08:22 +0000

    Launch screen changing

--- PinkRoomChallenge/Main/MainView.swift
+++ PinkRoomChallenge/Main/MainView.swift
@@ -41,7 +41,7 @@ struct MainView: View {
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView(selectedSideMenuTab: 2)
+        MainView(selectedSideMenuTab: 0)
     }
 }
 
